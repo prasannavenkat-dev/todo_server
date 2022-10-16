74fdd5531bdf28ec7b9e0667e0d1083f7b08fc0a
unknown <prasannavenkat1998@yahoo.com>
2022-10-16 11:17:00 +0530

    Added cors option

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ app.use(bodyParser.json())
 
 
 const cors = require("cors");
+
+const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+
 app.use(cors(
-    {
-        origin:"*"
-    }
+    corsOptions
 ));
 
 const mongodb = require('mongodb');
@@ -23,7 +28,6 @@ require('dotenv').config()
 app.get("/getTodoList",async function(req,res){
 
     try {
-        console.log(process.env.DB_URL);
         const clientInfo = await mongoClient.connect(process.env.DB_URL);
         const db = await clientInfo.db("todo");
 
